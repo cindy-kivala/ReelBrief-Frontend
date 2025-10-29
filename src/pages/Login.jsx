cad0797b7e7aaecb1e20dbc24367e7124cec2062
monica <monica.njoki@student.moringaschool.com>
2025-10-29 22:11:57 +0300

    touch

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -5,7 +5,6 @@
  */
 
 import React from 'react';
-import ProjectList from '../pages/ProjectList';
 // TODO: Import and render LoginForm component
 // - Handle form submission
 // - Redirect on successful login
@@ -15,9 +14,6 @@ function Login() {
     <div>
       <h1>Login Page</h1>
       {/* <LoginForm /> will go here once implemented */}
-      <div>
-        < ProjectList/>
-      </div>
     </div>
   );
 }
